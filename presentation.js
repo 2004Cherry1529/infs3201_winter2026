6dce55fc070467613e62712e17ff31515f52a07a
sumaya <sumayaalaseri@gmail.com>
2026-02-12 21:21:40 +0300

    Presentation: displayEmployees with formatted table output

--- presentation.js
+++ presentation.js
@@ -1,8 +1,36 @@
 const business = require('./business.js');
 const prompt = require('prompt-sync')();
 
-console.log('=== Employee Scheduling System ===\n');
+/**
+ * Display all employees in a formatted table
+ */
+async function displayEmployees() {
+    console.log('\n' + '='.repeat(60));
+    console.log('EMPLOYEE LIST');
+    console.log('='.repeat(60));
+    
+    const employees = await business.getAllEmployees();
+    
+    if (employees.length === 0) {
+        console.log('No employees found.');
+        return;
+    }
+    
+    // Header
+    console.log('ID'.padEnd(10) + 'Name'.padEnd(25) + 'Phone');
+    console.log('-'.repeat(60));
+    
+    // Rows
+    for (const emp of employees) {
+        console.log(
+            emp.employeeId.padEnd(10) + 
+            emp.name.padEnd(25) + 
+            emp.phone
+        );
+    }
+    console.log('='.repeat(60) + '\n');
+}
 
 module.exports = {
-    // We'll add functions here
+    displayEmployees
 };
