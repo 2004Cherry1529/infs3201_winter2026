9d31100e47d9f0fad2389a5f65484a5dd349200e
sumaya <sumayaalaseri@gmail.com>
2026-02-12 21:12:42 +0300

    Presentation: initial setup with business require

---
+++ presentation.js
@@ -0,0 +1,8 @@
+const business = require('./business.js');
+const prompt = require('prompt-sync')();
+
+console.log('=== Employee Scheduling System ===\n');
+
+module.exports = {
+    // We'll add functions here
+};
