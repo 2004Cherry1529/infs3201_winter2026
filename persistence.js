8161567b2cfb45b20b17a9084fba865b3e62472c
sumaya <sumayaalaseri@gmail.com>
2026-02-12 19:37:57 +0300

    Persistence: writeEmployees function

--- persistence.js
+++ persistence.js
@@ -9,6 +9,14 @@ async function readEmployees() {
     return JSON.parse(data);
 }
 
+/**
+ * Writes employees array to employees.json
+ * @param {Array} employees - Array of employee objects
+ */
+async function writeEmployees(employees) {
+    await fs.writeFile('employees.json', JSON.stringify(employees, null, 4));
+}
 module.exports = {
-    readEmployees
+    readEmployees,
+    writeEmployees
 };
